2e2392b364df116b3adc8670d9e00b8abc40b21d
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-09-03 16:48:02 +0700

    About us

--- src/components/about_us/AboutUs.tsx
+++ src/components/about_us/AboutUs.tsx
@@ -1,7 +1,42 @@
+import Service from "./Service"
+
 const AboutUs: React.FC = () => {
     return (
-        <div></div>
+        <section className="pb-[165px] max-w-[1440px] mx-auto">
+            <div className="mx-24 text-center">
+                <h2 className="text-base font-bold">About us</h2>
+                <p className="mt-3 mb-12 text-gray">Order now and appreciate the beauty of nature</p>
+                <div className="grid grid-cols-3 gap-6">
+                    {
+                        services.map((service) => {
+                            return <Service
+                                icon={service.icon}
+                                title={service.title}
+                                desc={service.desc} />
+                        })
+                    }
+                </div>
+            </div>
+        </section>
     )
 }
 
 export default AboutUs
+
+const services = [
+    {
+        icon: "./icon/Vector1.svg",
+        title: "Large Assortment",
+        desc: "we offer many different types of products with fewer variations in each category.",
+    },
+    {
+        icon: "./icon/Vector2.svg",
+        title: "Fast & Free Shipping",
+        desc: "4-day or less delivery time, free shipping and an expedited delivery option.",
+    },
+    {
+        icon: "./icon/Vector3.svg",
+        title: "24/7 Support",
+        desc: "answers to any business related inquiry 24/7 and in real-time.",
+    }
+]
---
+++ src/components/about_us/Service.tsx
@@ -0,0 +1,20 @@
+interface ServiceProps {
+    icon: string;
+    title: string;
+    desc: string;
+}
+
+const Service: React.FC<ServiceProps> = (props) => {
+    const { icon, title, desc } = props;
+    return (
+        <div>
+            <div className="w-24 h-24 mx-auto bg-primary rounded-full flex">
+                <img className="m-auto" src={icon} alt="icon"/>
+            </div>
+            <h3 className="mt-6 mb-3 font-bold">{title}</h3>
+            <p className="text-gray max-w-[350px] mx-auto">{desc}</p>
+        </div>
+    )
+}
+
+export default Service;
