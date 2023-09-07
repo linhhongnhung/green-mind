2039e915c9b654fc7b82ba166d4ed11378fb3e09
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-09-07 13:15:29 +0700

    responsive Search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -20,15 +20,20 @@ const Search: React.FC = () => {
         <section className="max-w-[1440px]
                             mx-auto">
 
-            <div className="min-h-[512px] rounded-[24px]
+            <div className="min-h-[512px] max-w-[1280px] rounded-[24px]
                             mx-24 pl-12
                             bg-primary
-                            flex">
+                            flex
+                            max-lg:pr-12
+                            max-md:mx-12
+                            max-sm:mx-6 max-sm:px-6">
 
                 <div>
                     <Slide index={0} direction="down">
                         <h2 className="text-large font-extrabold leading-[64px]
-                                       mt-12 mb-6">
+                                       mt-12 mb-6
+                                       max-xl:text-[3rem]
+                                       max-sm:text-base">
                             Buy your dream plants
                         </h2>
                     </Slide>
@@ -58,16 +63,22 @@ const Search: React.FC = () => {
                     <button className="w-12 h-12 bg-primary rounded-[12px]
                                        absolute
                                        ml-[393px] mt-2
-                                       hover:brightness-[0.6] transition delay-100 duration-200">
+                                       hover:brightness-[0.6] transition delay-100 duration-200
+                                       max-xl:ml-[233px]">
                         <img className="m-auto" src="./icon/Search.svg" alt="search button" />
                     </button>
-                    <input className="w-[449px] h-16 rounded-[12px]
+                    <input className="xl:w-[449px] h-16 rounded-[12px]
                                       pl-[18px] pr-16
-                                      text-tiny
-                                      focus:ring-2 focus:ring-gray focus:outline-none transition"
+                                      xl:text-tiny
+                                      focus:ring-2 focus:ring-gray focus:outline-none transition
+                                      max-xl:w-[290px]"
                         placeholder="What are you looking for?" />
                 </div>
-                <img className="self-end ml-auto" src="./img/Group16.svg" alt="tree" />
+                <div className="w-full ml-auto
+                                self-end
+                                max-md:hidden">
+                    <img src="./img/Group16.svg" alt="tree" />
+                </div>
             </div>
         </section>
     )
