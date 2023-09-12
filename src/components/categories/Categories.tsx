36cb6d62bec8516508eabb19d8996468f6b14fca
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-09-12 11:15:50 +0700

    responsive CateGories

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -7,7 +7,9 @@ const Categories: React.FC = () => {
                 <div className="grid grid-cols-3 gap-24
                                 absolute
                                 mt-[135px] mx-24
-                                max-w-[1248px]">
+                                max-w-[1248px]
+                                max-xl:mx-12 max-xl:gap-10
+                                max-sm:mx-6 max-sm:grid-cols-1 max-sm:right-0 max-sm:left-0">
                     {
                         categories.map((category, index) => {
                             return <CategoryItem
@@ -22,7 +24,9 @@ const Categories: React.FC = () => {
             </div>
             <h2 className="text-base font-bold">Categories</h2>
             <p className="mt-3 mb-24 text-gray text-tiny">Find what you are looking for</p>
-            <div className="bg-primary min-h-[841px]" />
+            <div className="bg-primary h-[841px]
+                            max-xl:h-[650px]
+                            max-sm:h-[2300px]" />
         </section>
     )
 }
--- src/components/categories/CategoryItem.tsx
+++ src/components/categories/CategoryItem.tsx
@@ -25,18 +25,20 @@ const CategoryItem: React.FC<CategoryItemProps> = (props) => {
         <Link
             href={href}
             className="flex flex-col
-                       rounded-[16px] 
-                       hover:translate-y-[96px] transition duration-500"
+                       rounded-[16px]
+                       lg:hover:translate-y-[96px] max-lg:translate-y-[96px] transition duration-500"
             onMouseEnter={handleHover}
             onMouseLeave={handleNoHover}>
             <div>
-                <img className="max-w-[352px]" src={img} alt="plants" />
+                <div>
+                    <img className="mx-auto" src={img} alt="plants" />
+                </div>
                 <h3 className="font-bold text-tiny my-3">{name}</h3>
             </div>
             <div className={
                 isHoverd
-                    ? "opacity-1 transition duration-1000"
-                    : "opacity-0 transition duration-500"}>
+                    ? "lg:opacity-1 transition duration-1000"
+                    : "lg:opacity-0 transition duration-500"}>
                 <p className="text-gray text-tiny mb-6">{desc}</p>
                 <button className="w-[147px] h-[51px]
                                    rounded-[8px] bg-white
